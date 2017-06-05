2300f30c31a04b0eee37e9801f5bc8a921ab9dcb
nt1st <t.nandan2008@gmail.com>
2017-06-05 19:45:49 -0400

    Added method to get imports

--- pythonsourcecodeunraveler/sfile.py
+++ pythonsourcecodeunraveler/sfile.py
@@ -6,14 +6,14 @@ Provides framework to access python program attributes
 """
 
 
-class SFile(object):  # pylint: disable=R0903
+class SFile(object):
     """
-    This class extracts classes, methods, and function signatures 
+    This class extracts classes, methods, and function signatures
     from a .py file
 
     Attributes:
-    _filelines(list): This is a list of strings, where each 
-                        item is a line of the source file
+        _filelines(list): This is a list of strings, where each
+            item is a line of the source file
     """
     def __init__(self, filepath):
         """This initializes an SCFile object
@@ -29,15 +29,40 @@ class SFile(object):  # pylint: disable=R0903
         del pyfile
 
     def get_imports(self):
-        """ Returns the classes/modules imported in a list format """
+        """ Get classes/modules imported in this module
+
+        Returns:
+            list: Classes/modules imported by this module
+
+        Raises:
+            SyntaxError:
+        """
+        retlist = []
         for line in self._filelines:
-            pass
+            linearray = line.split()
+            if 'import' in linearray and 'from' in linearray:
+                try:
+                    retlist.append(linearray[linearray.index('from') + 1] +
+                                   "." +
+                                   linearray[linearray.index('import')+1])
+                except IndexError:
+                    raise SyntaxError("The file selected has \
+invalid import statements.")
+            elif 'import' in linearray:
+                try:
+                    retlist.append(linearray[linearray.index('import')])
+                except IndexError:
+                    raise SyntaxError("The file selected has \
+invalid import statements.")
+            del linearray
+        return retlist
+        # TODO check if the import statement is inside a string literal
 
     def get_method_signatures(self):
         """ Returns the methods present in the module that do not belong to any classes
 
         Returns:
-            list: List of lists where the first item in each list is a method name, 
-                and the next is a list of arguments.
+            list: List of lists where the first item in each
+            list is a method name, and the next is a list of arguments.
         """
         raise NotImplementedError
