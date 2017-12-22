3e3a31bab22447fb5dcd2161247ec9d8ab669ba2
Nandan Tumu <10603428+nandantumu@users.noreply.github.com>
2017-12-22 18:02:39 -0500

    Imported all classes in __init__.py

--- pysourcegraph/__init__.py
+++ pysourcegraph/__init__.py
@@ -1,1 +1,7 @@
-from .nodes import BaseNode
+"""
+This package is a way for people who contribute to software to develop an
+understanding of the codebase they wish to contribute to in a visual, intuitive
+manner.
+"""
+from .nodes import BaseNode, PackageNode, ModuleNode, ClassNode, FunctionNode,\
+                   ImportNode
