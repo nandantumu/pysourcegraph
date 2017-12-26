d02846868e04de52fed977b65863c1a9a674dde7
Nandan Tumu <10603428+nandantumu@users.noreply.github.com>
2017-12-25 19:24:36 -0500

    Created testing suite

---
+++ test/nodes_test.py
@@ -0,0 +1,31 @@
+"""Test module for nodes.py"""
+import unittest
+from pysourcegraph import *
+from pysourcegraph.nodes import map_folder, map_module
+
+class NodesTest(unittest.TestCase):
+    """Test node attributes"""
+    def test_base_node_equality(self):
+        """Test equality and not equality of BaseNode"""
+        self.assertEqual(BaseNode("Test1"), BaseNode("Test1"))
+        self.assertEqual(BaseNode("Test1", "docstring here"),
+                         BaseNode("Test1", "docstring here"))
+        self.assertNotEqual(BaseNode("Test1"), BaseNode("Test2"))
+        self.assertNotEqual(BaseNode("Test1", "docstring"),
+                            BaseNode("Test1"))
+
+    def test_node_is_childless(self):
+        """Test is_childless method of BaseNode"""
+        self.assertTrue(BaseNode("Test1").is_childless())
+        node = BaseNode("Test1")
+        node.add_child(BaseNode("TestChild"))
+        self.assertFalse(node.is_childless())
+
+class TreeCreationTest(unittest.TestCase):
+    """Test the creation of trees from source"""
+    def test_tree_builder(self):
+        self.assertIsNotNone(tree_builder(r"C:\Users\nanda\Downloads\astroid-1.6.0"))
+
+
+if __name__ == '__main__':
+    unittest.main()
